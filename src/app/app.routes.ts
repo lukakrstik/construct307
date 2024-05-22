dd0454cacdf349a7bdb0062c0571bdbfb177779c
Luka Krstikj <luka.krstik@protonmail.com>
2024-05-22 21:09:29 +0200

    added projects and aboutus components and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {AdminDashboardComponent} from "./admin-dashboard/admin-dashboard.component";
 import {HomeComponent} from "./home/home.component";
+import {ProjectsComponent} from "./projects/projects.component";
+import {AboutusComponent} from "./aboutus/aboutus.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: "full"},
   {path: 'ap', component: LoginComponent},
   {path: 'apdash', component: AdminDashboardComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'aboutus', component: AboutusComponent}
 ];
