d31ec4df0877518b2f00e77c17bdd820814a22e5
Luka Krstikj <luka.krstik@protonmail.com>
2024-05-27 00:41:22 +0200

    projects component navbar added

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
