68832fb2be9d0e5870f138625ae7e99689a14996
Luka Krstikj <luka.krstik@protonmail.com>
2024-05-27 00:40:00 +0200

    aboutus component early code started

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+import {CommonModule} from "@angular/common";
+import {FooterComponent} from "../footer/footer.component";
 
 @Component({
   selector: 'app-aboutus',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, CommonModule, FooterComponent],
   templateUrl: './aboutus.component.html',
   styleUrl: './aboutus.component.css'
 })
