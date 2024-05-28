90b70f17cac51c785e0db7d5d803f097ace653ec
Luka Krstikj <luka.krstik@protonmail.com>
2024-05-28 22:55:52 +0200

    detailed view page routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import {AdminDashboardComponent} from "./admin-dashboard/admin-dashboard.compone
 import {HomeComponent} from "./home/home.component";
 import {ProjectsComponent} from "./projects/projects.component";
 import {AboutusComponent} from "./aboutus/aboutus.component";
+import {ProjectDetailComponent} from "./project-detail/project-detail.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: "full"},
@@ -11,5 +12,6 @@ export const routes: Routes = [
   {path: 'apdash', component: AdminDashboardComponent},
   {path: 'home', component: HomeComponent},
   {path: 'projects', component: ProjectsComponent},
-  {path: 'about-us', component: AboutusComponent}
+  {path: 'about-us', component: AboutusComponent},
+  {path: 'project/:id', component: ProjectDetailComponent}
 ];
