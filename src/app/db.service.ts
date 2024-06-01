99d9d539768180dcc513c5789b9866982c20f6f3
Luka Krstikj <luka.krstik@protonmail.com>
2024-06-01 02:29:46 +0200

    pushProject function added

--- src/app/db.service.ts
+++ src/app/db.service.ts
@@ -1,6 +1,8 @@
 import {inject, Injectable} from '@angular/core';
-import {collection, doc, Firestore, getDoc, getDocs, query} from "@angular/fire/firestore";
+import {addDoc, collection, doc, Firestore, getDoc, getDocs, query, setDoc} from "@angular/fire/firestore";
 import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from "@angular/fire/compat/firestore";
+import {Project} from "./project";
+import {Floorplan} from "./floorplan";
 
 @Injectable({
   providedIn: 'root'
@@ -19,4 +21,20 @@ export class DbService {
       return (await getDoc(doc(this.db, "projects", id))).data();
   }
 
+  async pushProject(p: Project){
+    const floorplans = p.floorplan.map((obj) => {return Object.assign({}, obj)})
+    let docData = {
+      name: p.name,
+      address: p.address,
+      desc: p.desc,
+      photo: p.photo,
+      id: p.id,
+      noApartments: p.noApartments,
+      floors: p.floors,
+      svgPaths: p.svgPaths,
+      floorplan: floorplans
+    }
+    console.log(docData, docData.floorplan)
+    await setDoc(doc(this.db, "projects",p.id), docData).catch(error => console.log(error));
+  }
 }
