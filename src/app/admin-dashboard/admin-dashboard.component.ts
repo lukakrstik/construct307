1f159c57483a73af49df45f9030774721684970e
Luka Krstikj <luka.krstik@protonmail.com>
2024-06-01 02:33:13 +0200

    admin dashboard work started

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,20 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../auth.service";
-import {Router} from "@angular/router";
+import {Router, RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
+import {CommonModule} from "@angular/common";
+import {DbService} from "../db.service";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
-export class AdminDashboardComponent {
-  constructor(private authService: AuthService, private router: Router) {
+export class AdminDashboardComponent implements OnInit{
+  constructor(private authService: AuthService, private router: Router, private dbService: DbService) {
+  }
+
+  user ?: firebase.default.User | null ;
+  public projects : any;
+  async readData() {
+    this.projects = await this.dbService.getData();
   }
 
   logout(){
     this.authService.logout();
     this.router.navigate(['/home']);
   }
+
+  redirectToLogin(){
+    this.router.navigate(['/ap'])
+  }
+  ngOnInit() {
+    this.authService.auth.user.subscribe(u => {
+      this.user = u;
+      if(!this.user){
+        this.router.navigate(['/ap'])
+      }
+    })
+    this.readData().then(() => {
+      console.log(this.projects[0].name)
+    });
+  }
+
 }
